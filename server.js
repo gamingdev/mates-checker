00a0dc7874acc13eb18a1d8964a3e5f1be0432c3
gamingdev <gamingdev.net@gmail.com>
2016-02-26 23:08:54 +0100

    add advertising on default page

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ server.use(vhost('mates-checker.'+config.env.dns, require('./mates-checker/index
 
 server.get('/', function(req, res) {
 
-    res.end('<html><body>More tools incoming</body></html>');
+    var script = '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script><ins class="adsbygoogle" style="display:inline-block;width:300px;height:600px" data-ad-client="ca-pub-5466650298483971" data-ad-slot="7564072442"></ins><script>(adsbygoogle = window.adsbygoogle || []).push({});</script>';
+
+    res.end('<html><body><p>More tools incoming</p>' + script + '</body></html>');
 });
 
 module.exports = server;
